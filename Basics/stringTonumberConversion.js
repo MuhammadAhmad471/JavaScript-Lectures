c803b1780691db0a08c17a37cb061c33ce7b503a
Muhammad Ahmad <141666657+MuhammadAhmad471@users.noreply.github.com>
2024-01-24 08:25:18 +0000

    Conversion string and number

---
+++ Basics/stringTonumberConversion.js
@@ -0,0 +1,15 @@
+console.log(2+2);
+console.log(2-2);
+console.log(3/20);
+let val=10;
+let nagVal=-val;
+console.log(nagVal);
+console.log("1"+"2");
+console.log(2+"3");
+console.log("2"+2+2);
+console.log(2+2+"2");//its a role to add number to string
+console.log("2"+(2+2));
+let str="Ahmad ";
+let str1=  123;
+console.log(str+str1);
+console.log((2+3)+(4/2)/(2*4)*(4-3));
