e39403f1977e253119c1980cebe17a57ceaa01c1
Muhammad Ahmad <141666657+MuhammadAhmad471@users.noreply.github.com>
2024-01-24 20:49:35 +0500

    Update stringTonumberConversion.js

--- Basics/stringTonumberConversion.js
+++ Basics/stringTonumberConversion.js
@@ -13,3 +13,6 @@ let str="Ahmad ";
 let str1=  123;
 console.log(str+str1);
 console.log((2+3)+(4/2)/(2*4)*(4-3));
+let z=10;
+console.log(z++)//It prints the origional value 10 then increment 1 in z value when we print z next time we see value is 11
+console.log(++z) //It increment the value of z then print its value result in this case is 11
