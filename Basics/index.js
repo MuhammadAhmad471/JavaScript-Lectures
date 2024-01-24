ddd139f215a3c3ec53b903aefbeb95e190bba694
Muhammad Ahmad <141666657+MuhammadAhmad471@users.noreply.github.com>
2024-01-24 11:50:21 +0500

    Update index.js

--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,36 @@
 console.log("Ahmad");
+let scrore="20";
+let score3="30klm"
+let valueInNumber3=Number(score3);
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+console.log(typeof scrore); //string
+let score1=null;
+console.log(typeof score1); //object
+let score2=undefined;
+console.log(typeof score2);//undefined
+let valueInNumber=Number(score1)
+console.log(typeof valueInNumber);//Always number
+console.log(valueInNumber);//0
+let valueInNumber2=Number(score2);
+console.log(typeof valueInNumber2);//Number
+let score4=undefined;
+let valueInNumber4=Number(score4);
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4);
+let scrore5=false;
+console.log(typeof scrore5);
+console.log(scrore5);
+let valueInNumber5=Number(scrore5);
+console.log(typeof valueInNumber5);
+console.log(valueInNumber5);
+//"33"=> 33 number
+//"33abac" number came after log and vale is= NaN
+let isloggedIn=0;
+let booleanIslogedIn=Boolean(isloggedIn);
+console.log(typeof booleanIslogedIn);
+console.log(booleanIslogedIn);
+let someNumber=40;
+let stringsomeNumber=String(someNumber);
+console.log(typeof stringsomeNumber);
+console.log(stringsomeNumber);
